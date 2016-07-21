8817d153ca7a0d02142f9988ae9da47929bc5bba
Craig Watson <craig@bashton.com>
2016-07-21 11:53:44 +0100

    TF: Initial commit

---
+++ 10-vars.tf
@@ -0,0 +1,4 @@
+variable "destionation_cidr"  {}
+variable "peer_connection_id" {}
+variable "public_route_table" {}
+variable "nat_route_tables"   {}
---
+++ 20-routes.tf
@@ -0,0 +1,12 @@
+resource "aws_route" "public" {
+  destination_cidr_block    = "${var.destionation_cidr}"
+  vpc_peering_connection_id = "${var.peer_connection_id}"
+  route_table_id            = "${var.public_route_table}"
+}
+
+resource "aws_route" "nat" {
+  count                     = "${length(split(",", var.nat_route_tables))}"
+  destination_cidr_block    = "${var.destionation_cidr}"
+  vpc_peering_connection_id = "${var.peer_connection_id}"
+  route_table_id            = "${element(split(",", var.nat_route_tables), count.index)}"
+}
