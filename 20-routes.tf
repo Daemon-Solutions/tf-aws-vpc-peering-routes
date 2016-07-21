887d97ef6db4ea69a6162c4eb00b53c31be12879
Craig Watson <craig@bashton.com>
2016-07-21 12:05:13 +0100

    Naming

--- 20-routes.tf
+++ 20-routes.tf
@@ -1,10 +1,10 @@
-resource "aws_route" "public" {
+resource "aws_route" "peer_public" {
   destination_cidr_block    = "${var.destionation_cidr}"
   vpc_peering_connection_id = "${var.peer_connection_id}"
   route_table_id            = "${var.public_route_table}"
 }
 
-resource "aws_route" "nat" {
+resource "aws_route" "peer_nat" {
   count                     = "${length(split(",", var.nat_route_tables))}"
   destination_cidr_block    = "${var.destionation_cidr}"
   vpc_peering_connection_id = "${var.peer_connection_id}"
