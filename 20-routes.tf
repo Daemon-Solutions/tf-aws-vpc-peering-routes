24d60a4b2cc2f83d031d920ce7ad2f30e96d34d6
Craig Watson <craig@bashton.com>
2016-07-21 12:18:08 +0100

    Hacky workaround for count dependency bugs

--- 20-routes.tf
+++ 20-routes.tf
@@ -4,9 +4,25 @@ resource "aws_route" "peer_public" {
   route_table_id            = "${var.public_route_table}"
 }
 
-resource "aws_route" "peer_nat" {
-  count                     = "${length(split(",", var.nat_route_tables))}"
+/*
+ * Hacky workaround is hacky - see:
+ * https://github.com/hashicorp/terraform/issues/2301
+ * https://github.com/hashicorp/terraform/issues/7741
+ */
+resource "aws_route" "peer_nat_a" {
   destination_cidr_block    = "${var.destionation_cidr}"
   vpc_peering_connection_id = "${var.peer_connection_id}"
-  route_table_id            = "${element(split(",", var.nat_route_tables), count.index)}"
+  route_table_id            = "${element(split(",", var.nat_route_tables), 0)}"
+}
+
+resource "aws_route" "peer_nat_b" {
+  destination_cidr_block    = "${var.destionation_cidr}"
+  vpc_peering_connection_id = "${var.peer_connection_id}"
+  route_table_id            = "${element(split(",", var.nat_route_tables), 1)}"
+}
+
+resource "aws_route" "peer_nat_c" {
+  destination_cidr_block    = "${var.destionation_cidr}"
+  vpc_peering_connection_id = "${var.peer_connection_id}"
+  route_table_id            = "${element(split(",", var.nat_route_tables), 2)}"
 }
